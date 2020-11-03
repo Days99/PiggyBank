2056399c3103cc3cfc573fac4cfc8449975e0dcd
okkgg9 <62141137+okkgg9@users.noreply.github.com>
2020-11-03 00:44:34 +0000

    PiggyBank dynamic AI movement

---
+++ PiggyBank/Assets/Scripts/Piggy Scripts/ConnectedWaypoint.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectedWaypoint : WaypointScript
+{
+    [SerializeField] protected float connectivityRadius = 10.0f;
+    List<ConnectedWaypoint> connections;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] allWayPoints = GameObject.FindGameObjectsWithTag("Waypoint");
+        connections = new List<ConnectedWaypoint>();
+
+        for(int i = 0; i < allWayPoints.Length; i++)
+        {
+            ConnectedWaypoint nextWaypoint = allWayPoints[i].GetComponent<ConnectedWaypoint>();
+
+            if(nextWaypoint != null)
+            {
+                if(Vector3.Distance(this.transform.position, nextWaypoint.transform.position) <= connectivityRadius && nextWaypoint != this)
+                {
+                    connections.Add(nextWaypoint);
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, debugDrawRadius);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, connectivityRadius);
+    }
+
+    public ConnectedWaypoint NextWayPoint(ConnectedWaypoint previousWaypoint)
+    {
+        if(connections.Count == 0)
+        {
+            return null;
+        }
+        else if(connections.Count == 1 && connections.Contains(previousWaypoint))
+        {
+            return previousWaypoint;
+        }
+        else
+        {
+            ConnectedWaypoint nextWaypoint;
+            int nextIndex = 0;
+
+            do
+            {
+                nextIndex = UnityEngine.Random.Range(0, connections.Count);
+                nextWaypoint = connections[nextIndex];
+            } while (nextWaypoint == previousWaypoint);
+
+            return nextWaypoint;
+        }
+    }
+}
--- PiggyBank/Assets/Scripts/Piggy Scripts/PiggyBankMovement.cs
+++ PiggyBank/Assets/Scripts/Piggy Scripts/PiggyBankMovement.cs
@@ -6,36 +6,100 @@ using UnityEngine.AI;
 
 public class PiggyBankMovement : MonoBehaviour
 {
-    [SerializeField] Transform destination;
+    [SerializeField] bool patrolWaiting;
+    [SerializeField] float totalWaitTime = 1f;
+    //probability of switching direction
+    [SerializeField] float switchProbability = 0.2f;
 
-    NavMeshAgent agent;
+    NavMeshAgent navMeshAgent;
+    ConnectedWaypoint currentWaypoint;
+    ConnectedWaypoint previousWaypoint;
 
+    bool travelling;
+    bool waiting;
+    float waitTimer;
+    int waypointsVisited;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
 
-        agent = this.GetComponent<NavMeshAgent>();
-
-        if (agent == null)
+        if (navMeshAgent == null)
         {
             Debug.LogError("yo navMesh agent not attached to" + gameObject.name);
-        }
-        else
+        } else
         {
-            SetDestination();
+            if(currentWaypoint == null)
+            {
+                GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("Waypoint");
 
+                if(allWaypoints.Length > 0)
+                {
+                    while(currentWaypoint == null)
+                    {
+                        int random = UnityEngine.Random.Range(0, allWaypoints.Length);
+                        ConnectedWaypoint startingWaypoint = allWaypoints[random].GetComponent<ConnectedWaypoint>();
+
+                        if (startingWaypoint != null)
+                        {
+                            currentWaypoint = startingWaypoint;
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Failed to find any waypoints for use in the scene.");
+                }
+            }
+
+            SetDestination();
+        }
+
+    }
+
+
+    public void Update()
+    {
+        //check if close to destination
+        if (travelling && navMeshAgent.remainingDistance <= 1.0f)
+        {
+            travelling = false;
+            waypointsVisited++;
+
+            if (patrolWaiting)
+            {
+                waiting = true;
+                waitTimer = 0f;
+            }
+            else
+            {
+                SetDestination();
+            }
+        }
+
+        if (waiting)
+        {
+            waitTimer += Time.deltaTime;
+            if(waitTimer >= totalWaitTime)
+            {
+                waiting = false;
+                SetDestination();
+            }
         }
     }
+
 
     private void SetDestination()
     {
-        if (destination != null)
+        if(waypointsVisited > 0)
         {
-            Vector3 targetVector = destination.transform.position;
-            agent.SetDestination(targetVector);
-            Debug.Log(destination.transform.position);
+            ConnectedWaypoint nextWaypoint = currentWaypoint.NextWayPoint(previousWaypoint);
+            previousWaypoint = currentWaypoint;
+            currentWaypoint = nextWaypoint;
         }
-    }
 
+        Vector3 targetVector = currentWaypoint.transform.position;
+        navMeshAgent.SetDestination(targetVector);
+        travelling = true;
+    }
 }
